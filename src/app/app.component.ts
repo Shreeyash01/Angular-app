43a0f2181464dcfbed8fd5d77a2b113ee9576acb
Shreeyash <shreeyash545@gmail.com>
2023-04-24 23:47:22 +0530

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'my-app';
+  currentTime: string;
+
+  constructor() {
+    this.updateTime();
+    setInterval(() => this.updateTime(), 1000);
+  }
+
+  private updateTime() {
+    const date = new Date();
+    const hours = this.formatNumber(date.getHours());
+    const minutes = this.formatNumber(date.getMinutes());
+    const seconds = this.formatNumber(date.getSeconds());
+    this.currentTime = `${hours}:${minutes}:${seconds}`;
+  }
+
+  private formatNumber(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 }
